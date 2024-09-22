358066acc80588f7fab6074cef7b031adef404f5
DrBotic <ezralboerman@gmail.com>
2024-09-22 00:46:03 -0400

    setting up the environment

---
+++ Frontend/styles/search.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+import { COLORS, FONT, SIZES } from "../constants";
+
+
+const styles = StyleSheet.create({
+
+    //css goes in here
+
+})
+
+
+
+export default styles;
