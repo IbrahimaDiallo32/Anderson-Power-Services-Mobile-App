22bc169b6c260a14605e0f7145f215a78b899e7e
Greg <gregloj2001@gmail.com>
2024-10-10 15:29:03 -0400

    Set directory names to have NO dashes / no spaces

---
+++ apsbackend/src/test/java/com/apsbackend/apsbackend/ApsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apsbackend.apsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
