d78f1d510fcff1b450b8dc0e3aae735e4d8666cd
Greg <gregloj2001@gmail.com>
2024-10-28 23:30:54 -0400

    Create CustomUserDetails.java

---
+++ restfulbackend/src/main/java/com/aps/restfulbackend/Services/Auth/CustomUserDetails.java
@@ -0,0 +1,52 @@
+package com.aps.restfulbackend.Services.Auth;
+
+import com.aps.restfulbackend.Models.AppUser;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+
+    private AppUser appUser;
+
+    public CustomUserDetails(AppUser appUser) {
+        this.appUser = appUser;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
