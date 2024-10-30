2cf23b3f7fa61f524e848eba4b87f8ff9be13954
Greg <gregloj2001@gmail.com>
2024-10-30 14:10:12 -0400

    Introduce registerUser()

--- restfulbackend/src/main/java/com/aps/restfulbackend/Repositories/AppUserRepository.java
+++ restfulbackend/src/main/java/com/aps/restfulbackend/Repositories/AppUserRepository.java
@@ -1,6 +1,8 @@
 package com.aps.restfulbackend.Repositories;
 
 import com.aps.restfulbackend.Models.AppUser;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -13,4 +15,13 @@ public interface AppUserRepository extends CrudRepository<AppUser, UUID> {
 
     @Query(value = "SELECT * FROM app_user WHERE email = :email", nativeQuery = true)
     AppUser getUserByEmail(@Param("email") String email);
+
+    @Transactional
+    @Modifying
+    @Query(value = "INSERT INTO app_user (first_name, last_name, email, password, phone) VALUES(:first_name, :last_name, :email, :password, :phone)", nativeQuery = true)
+    int registerUser(@Param("first_name") String first_name,
+                     @Param("last_name") String last_name,
+                     @Param("email") String email,
+                     @Param("password") String password,
+                     @Param("phone") String phone);
 }
