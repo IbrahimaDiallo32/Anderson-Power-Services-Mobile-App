02aaaa9a0ea1bfe513604d0bbeda775fe868396b
Greg <gregloj2001@gmail.com>
2024-10-29 20:57:32 -0400

    Remove unneeded imports

--- restfulbackend/src/main/java/com/aps/restfulbackend/Controllers/AppUserController.java
+++ restfulbackend/src/main/java/com/aps/restfulbackend/Controllers/AppUserController.java
@@ -1,8 +1,5 @@
 package com.aps.restfulbackend.Controllers;
 
-import com.aps.restfulbackend.Services.AppUserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
