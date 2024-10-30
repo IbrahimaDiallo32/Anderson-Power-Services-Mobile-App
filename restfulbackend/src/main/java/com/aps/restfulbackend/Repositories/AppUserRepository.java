9b4ec9bb96ca103d63f02fc63784428819ab7f77
Greg <gregloj2001@gmail.com>
2024-10-29 20:05:21 -0400

    Update AppUserRepository.java

--- restfulbackend/src/main/java/com/aps/restfulbackend/Repositories/AppUserRepository.java
+++ restfulbackend/src/main/java/com/aps/restfulbackend/Repositories/AppUserRepository.java
@@ -1,10 +1,16 @@
 package com.aps.restfulbackend.Repositories;
 
 import com.aps.restfulbackend.Models.AppUser;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface AppUserRepository extends CrudRepository<AppUser, UUID> {
 
+    @Query(value = "SELECT * FROM app_user WHERE email = :email", nativeQuery = true)
+    AppUser getUserByEmail(@Param("email") String email);
 }
