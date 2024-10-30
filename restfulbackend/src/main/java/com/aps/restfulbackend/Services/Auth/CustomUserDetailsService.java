30b91e838c85f29b031a0f1f10b4b745fc183f99
Greg <gregloj2001@gmail.com>
2024-10-29 20:57:17 -0400

    Implement loadByUsername()

--- restfulbackend/src/main/java/com/aps/restfulbackend/Services/Auth/CustomUserDetailsService.java
+++ restfulbackend/src/main/java/com/aps/restfulbackend/Services/Auth/CustomUserDetailsService.java
@@ -1,5 +1,7 @@
 package com.aps.restfulbackend.Services.Auth;
 
+import com.aps.restfulbackend.Models.AppUser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,8 +10,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    private AppUserService appUserService;
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        AppUser appUser = appUserService.getUserByEmail(email);
+
+        // Check if user object is null
+        if (appUser == null) {
+            throw new UsernameNotFoundException("Unable to load user...");
+        }
+
+        return new CustomUserDetails(appUser);
     }
 }
