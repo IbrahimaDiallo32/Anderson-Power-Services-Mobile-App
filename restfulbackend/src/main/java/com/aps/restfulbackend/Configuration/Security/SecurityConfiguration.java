b7897e04375a62648db0af661ac35a06b7eb1838
Greg <gregloj2001@gmail.com>
2024-10-30 10:31:42 -0400

    Introduce authenticationProvider(), passwordEncoder(), and authenticationManager()

--- restfulbackend/src/main/java/com/aps/restfulbackend/Configuration/Security/SecurityConfiguration.java
+++ restfulbackend/src/main/java/com/aps/restfulbackend/Configuration/Security/SecurityConfiguration.java
@@ -1,15 +1,32 @@
 package com.aps.restfulbackend.Configuration.Security;
 
+import com.aps.restfulbackend.Configuration.Filters.JWTAuthFilter;
+import com.aps.restfulbackend.Services.Auth.CustomUserDetails;
+import com.aps.restfulbackend.Services.Auth.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Autowired
+    CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
+    JWTAuthFilter jwtAuthFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -20,6 +37,25 @@ public class SecurityConfiguration {
         .httpBasic(Customizer.withDefaults());
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(AuthenticationConfiguration authenticationConfiguration) {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(customUserDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(this.passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // This is where normal string password gets converted to a hashed password
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
 }
