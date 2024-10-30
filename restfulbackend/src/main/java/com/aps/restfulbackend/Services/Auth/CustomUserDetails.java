0f5a2b8f420a61df36dc0062868b171ff9b96626
Greg <gregloj2001@gmail.com>
2024-10-29 20:03:41 -0400

    Add proper functionality to return statements of CustomUserDetails methods

--- restfulbackend/src/main/java/com/aps/restfulbackend/Services/Auth/CustomUserDetails.java
+++ restfulbackend/src/main/java/com/aps/restfulbackend/Services/Auth/CustomUserDetails.java
@@ -17,17 +17,17 @@ public class CustomUserDetails implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return null;
     }
 
     @Override
     public String getPassword() {
-        return "";
+        return appUser.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return "";
+        return appUser.getEmail();
     }
 
     @Override
