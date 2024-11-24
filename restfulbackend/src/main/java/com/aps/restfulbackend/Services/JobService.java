c3b11822edf0cb1b01a39c8c2962478ef8eb4bb2
wawelgreg <141285799+wawelgreg@users.noreply.github.com>
2024-11-23 19:56:39 -0500

    Introduce class RegisterJob()

--- restfulbackend/src/main/java/com/aps/restfulbackend/Services/JobService.java
+++ restfulbackend/src/main/java/com/aps/restfulbackend/Services/JobService.java
@@ -4,9 +4,15 @@ import com.aps.restfulbackend.Repositories.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class JobService {
 
     @Autowired
     private JobRepository jobRepository;
+
+    public int RegisterJob(String job_id, UUID user_id, String auth_email) {
+        return 1;
+    }
 }
