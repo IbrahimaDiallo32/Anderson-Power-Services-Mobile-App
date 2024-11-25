9ef251a05d6fc1f55b988512fe2ba807a2f39e6b
wawelgreg <141285799+wawelgreg@users.noreply.github.com>
2024-11-24 21:49:44 -0500

    Add getters and setters for representative model

--- restfulbackend/src/main/java/com/aps/restfulbackend/Models/Representative.java
+++ restfulbackend/src/main/java/com/aps/restfulbackend/Models/Representative.java
@@ -11,4 +11,44 @@ public class Representative {
     private String last_name;
     private String phonenumber;
     private String rep_type;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getRep_type() {
+        return rep_type;
+    }
+
+    public void setRep_type(String rep_type) {
+        this.rep_type = rep_type;
+    }
 }
